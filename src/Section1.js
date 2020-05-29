095fb48e3a1366adc4993982012e16151c8aa673
muhdarifrawi <muhdarifrawi@gmail.com>
2020-05-29 13:02:49 +0800

    moved onClick event from div to button. added checkbox.

--- src/Section1.js
+++ src/Section1.js
@@ -7,14 +7,20 @@ function clickedEvent(){
     alert("hello there")
 }
 
+function tickEvent(){
+    
+}
 
 function Section1(){
     return(
-        <div className="Section1" onClick={clickedEvent}>
+        <div className="Section1">
             <h1>This is a test</h1>
-            <Button variant="contained" color="primary">
+            <Button variant="contained" color="primary" onClick={clickedEvent}>
             Hello World
             </Button>
+            <label>Checkbox</label>
+            <input type="checkbox" onClick={tickEvent}>
+            </input>
         </div>
     );
 }
