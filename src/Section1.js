f8833c4d91fa0a2b33bbe6c7b28e27cd865d7595
muhdarifrawi <muhdarifrawi@gmail.com>
2020-05-29 21:28:48 +0800

    checkbox is able to toggle checked/unchecked. However, value is still undefined.

--- src/Section1.js
+++ src/Section1.js
@@ -3,26 +3,35 @@ import './Section1.css';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 
-function clickedEvent(){
-    alert("hello there")
-}
 
-function tickEvent(){
+export default class Section1 extends React.Component
+{
+
+    constructor(props){
+        super(props);
+        this.state={
+            tickBox: false,
+        };
+        this.onTickEvent = this.onTickEvent.bind(this);
+    }
     
-}
-
-function Section1(){
-    return(
+    onTickEvent(event){
+        this.setState({tickBox: event.target.tickBox});
+        console.log("hey there" + this.state.tickBox);
+    }
+ 
+    render()
+    {
+        return(
         <div className="Section1">
             <h1>This is a test</h1>
-            <Button variant="contained" color="primary" onClick={clickedEvent}>
+            <Button variant="contained" color="primary">
             Hello World
             </Button>
             <label>Checkbox</label>
-            <input type="checkbox" onClick={tickEvent}>
+            <input type="checkbox" name="tickBox" checked={this.state.tickBox} onChange={this.onTickEvent} >
             </input>
         </div>
-    );
+        )
+    }
 }
-
-export default Section1;
