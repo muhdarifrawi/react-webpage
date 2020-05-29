5e7207d7feabf86fa497b69acd7fd160f26eaa37
muhdarifrawi <muhdarifrawi@gmail.com>
2020-05-29 12:54:53 +0800

    added onClick function to button.

--- src/Section1.js
+++ src/Section1.js
@@ -3,9 +3,14 @@ import './Section1.css';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 
+function clickedEvent(){
+    alert("hello there")
+}
+
+
 function Section1(){
     return(
-        <div className="Section1">
+        <div className="Section1" onClick={clickedEvent}>
             <h1>This is a test</h1>
             <Button variant="contained" color="primary">
             Hello World
