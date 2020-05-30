ae32926d2b7e66e491d42cab7dbdc0c3939f46e3
muhdarifrawi <muhdarifrawi@gmail.com>
2020-05-30 11:30:15 +0800

    added mock API secret for testing.

--- src/axios.js
+++ src/axios.js
@@ -29,7 +29,12 @@ export default class Axios extends React.Component
         for (const each in this.state.items){
             console.log(each);
         }
-        
+
+        // ideally this is not the best way to keep secrets. this is meant for dev stage only. 
+        // people can use devtool to actually read youe .env files in React
+        const API_KEY = process.env.REACT_APP_GOOGLE_PLACES_API_KEY;
+        console.log(API_KEY);
+
         return(
         <div className="axios">
             <h1>This is an axios</h1>
