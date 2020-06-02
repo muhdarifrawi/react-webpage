af037bb24e99869874797d44ab65fad4624d01bb
muhdarifrawi <muhdarifrawi@gmail.com>
2020-06-02 11:38:37 +0800

    successfully added and rendered React list component.

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
+
+export function SimpleList() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      
+      <List component="nav" aria-label="secondary mailbox folders">
+        <ListItem button>
+          <ListItemText primary="Trash" />
+        </ListItem>
+        <ListItemLink href="#simple-list">
+          <ListItemText primary="Spam" />
+        </ListItemLink>
+      </List>
+    </div>
+  );
+}
 
 
 export default class Axios extends React.Component
 {
+
+  
 
     constructor(props){
         super(props);
@@ -14,7 +51,14 @@ export default class Axios extends React.Component
         }
     }
 
-
+    // toggleDrawer(){
+    //   this.setState({
+    //     anchor:"",
+    //     state:""
+    //   })
+    // }
+   
+    
     
 
     componentDidMount() {
@@ -42,22 +86,29 @@ export default class Axios extends React.Component
  
     render()
     {   
-        const { error, isLoaded, items } = this.state;
+      
+      const { error, isLoaded, items } = this.state;
+
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
-        <ul>
-          {items.results.map(item => (
-            <li key={item.name}>
-              <h3>{item.name}</h3> 
-              <p>{item.formatted_address}</p> 
-              <p>{item.geometry.location.lat}, {item.geometry.location.lng}</p>
-            </li>
-          ))}
-        </ul>
+        <div>
+          
+          <SimpleList/>
+        
+          <ul>
+            {items.results.map(item => (
+              <li key={item.name}>
+                <h3>{item.name}</h3> 
+                <p>{item.formatted_address}</p> 
+                <p>{item.geometry.location.lat}, {item.geometry.location.lng}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       );
     }
     }
